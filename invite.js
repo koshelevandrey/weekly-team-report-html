27b8a645457d4e073f9a22ca271ee7f9202b9bd2
Realnaru <73569436+Realnaru@users.noreply.github.com>
2021-06-01 16:28:56 +0000

    Deploying to gh-pages-4 from @ codemakeracademy/weekly-team-report-html@811832b6a28ff23c7a2d826c5ebc61fc8f32915a 🚀

---
+++ invite.js
@@ -0,0 +1,1 @@
+!function(){document.getElementById("invite-form").addEventListener("submit",(function(n){n.preventDefault(),e()}));var e=function(){var e=document.getElementById("success-invite");document.querySelectorAll("input").forEach((function(e){e.value=null})),e.classList.remove("d-none")}}();
