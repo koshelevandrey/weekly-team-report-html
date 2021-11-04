14913c57e2e710e934adca3d4f124c89bf817344
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-04 14:01:34 +0300

    feat: changed bootstrap to 5V

--- src/my_reports.js
+++ src/my_reports.js
@@ -1,1 +1,7 @@
 import "./styles/main.scss";
+import "./img/happy.svg";
+import "./img/very-happy.svg";
+import "./img/very-sad.svg";
+import "./img/neutral.svg";
+import "./img/sad.svg";
+import "./img/unmarked.svg";
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ const isProduction = process.env.NODE_ENV === "production";
 const stylesHandler = "style-loader";
 const pages = [
   "my_reports",
-  "weekly_report_history"
+  "weekly_report_history",
+  "invite_your_team",
 ];
 const config = {
   entry: pages.reduce((config, page) => {
@@ -56,11 +57,9 @@ const config = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
+        type: "asset/resource",
+        use: 'file-loader?name=./[name].[ext]'
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
